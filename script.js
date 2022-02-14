2bb0cdf7c7642acdc182ff3b6c63db038363fa0c
Khalinadenae <96637309+Khalinadenae@users.noreply.github.com>
2022-02-14 16:29:33 -0500

    Imported the index.html, styles.css and scripts.js

---
+++ script.js
@@ -0,0 +1,100 @@
+// Feature #1: display the current date and time : Tuesday 16:00
+let now = new Date();
+
+let h4 = document.querySelector("h4");
+let hour = now.getHours();
+let minute = now.getMinutes();
+
+let time = `${hour}: ${minute}`;
+
+h4.innerHTML = `${time}`;
+
+let h2 = document.querySelector("h2");
+
+let days = [
+  "Sunday",
+  "Monday",
+  "Tuesday",
+  "Wednesday",
+  "Thursday",
+  "Friday",
+  "Saturday",
+];
+
+let months = [
+  "January",
+  "February",
+  "March",
+  "April",
+  "May",
+  "June",
+  "July",
+  "August",
+  "September",
+  "October",
+  "November",
+  "December",
+];
+
+let day = days[now.getDay()];
+let month = months[now.getMonth()];
+let date = now.getDate();
+
+h2.innerHTML = ` ${day} , ${month} , ${date}`;
+
+//Feature 2: when searching for a city (i.e. Paris), display the city name on the page after the user submits the form.
+
+function searchCity(event) {
+  event.preventDefault();
+  let searchInput = document.querySelector("#search-input");
+  let h1 = document.querySelector("h1");
+  h1.innerHTML = `${searchInput.value}`;
+
+  let apiKey = "7a8fb47ff40cd23384da3446c5066c54";
+  let unit = "imperial";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${searchInput.value}&appid=${apiKey}&units=${unit}`;
+  axios.get(apiUrl).then(showTemperature);
+}
+
+let form = document.querySelector("#search-form");
+form.addEventListener("submit", searchCity);
+
+//when a user searches for a city  it should display the current temperature of the city.
+
+function showTemperature(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#tempCF");
+  temperatureElement.innerHTML = `${temperature}°F`;
+
+  //temperature description
+  let maxTemperature = Math.round(response.data.main.temp_max);
+  let dailyMaxTemp = document.querySelector("#max-temperature");
+  dailyMaxTemp.innerHTML = `${maxTemperature}°F`;
+
+  let minTemperature = Math.round(response.data.main.temp_min);
+  let dailyMinTemp = document.querySelector("#min-temperature");
+  dailyMinTemp.innerHTML = ` ${minTemperature}°F`;
+
+  //Feels like
+  let feelsLike = Math.round(response.data.main.feels_like);
+  let perception = document.querySelector("#feels-like");
+  perception.innerHTML = `FEELS LIKE ${feelsLike}°F`;
+
+  //Humidity
+  let humidity = Math.round(response.data.main.humidity);
+  let showHumidity = document.querySelector("#humidity");
+  showHumidity.innerHTML = `${humidity}%`;
+
+  //Wind Speed
+  let windSpeed = Math.round(response.data.wind.speed);
+  let showWindSpeed = document.querySelector("#wind-speed");
+  showWindSpeed.innerHTML = `${windSpeed}mph`;
+}
+
+function currentLocation(response) {
+  let temperature = Math.round(response.data.main.temp);
+  let temperatureElement = document.querySelector("#tempCF");
+  temperatureElement.innerHTML = `${temperature}°F`;
+}
+
+//Add a Current Location button. When clicking on it, it uses the Geolocation API to get your GPS coordinates and display and the city and current temperature using the OpenWeather API.
